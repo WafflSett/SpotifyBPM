1ed14ea76825f20b3e60c034fca0b075273978d9
WaffleTG <toros.gergo@students.jedlik.eu>
2025-11-11 17:09:16 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SpotifyBPM
+{
+    public partial class App : Application
+    {
+        
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SpotifyBPM.ViewModels;
+
+namespace SpotifyBPM.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainViewModel mv)
+        {
+            InitializeComponent();
+            this.BindingContext = mv;
+        }
+
+        
+    }
+
+}
