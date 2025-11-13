119b30748785e67e621583371377c81e4ea25c2a
WaffleTG <toros.gergo@students.jedlik.eu>
2025-11-13 19:33:34 +0100

    token request works

--- Classes/HttpCommunication.cs
+++ Classes/HttpCommunication.cs
@@ -14,10 +14,15 @@ namespace FlagGame.Classes
         public async static Task<TokenResponse>? RequestAccessToken(string code, string redirectUri, string clientId, string codeVerifier)
         {
             using var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
-            Dictionary<string,string> dict = new Dictionary<string, string>();
-            var request = new HttpRequestMessage(HttpMethod.Post, (new UriBuilder($"https://accounts.spotify.com/api/token?grant_type=authorization_code&code={code}&redirect_uri={redirectUri}&client_id={clientId}&code_verifier={codeVerifier}")).Uri.AbsoluteUri) { Content = new FormUrlEncodedContent(dict)};
-            using var response = await client.SendAsync(request); // .ConfigureAwait(false)
+            var data = new[] {
+                new KeyValuePair<string, string>("client_id", clientId),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("redirect_uri", redirectUri),
+                new KeyValuePair<string, string>("code_verifier", codeVerifier),
+                new KeyValuePair<string, string>("grant_type", "authorization_code")
+            };
+            var content = new FormUrlEncodedContent(data);
+            using var response = await client.PostAsync($"https://accounts.spotify.com/api/token", content); // .ConfigureAwait(false)
             if (response.IsSuccessStatusCode)
             {
                 string resultString = response.Content.ReadAsStringAsync().Result;
